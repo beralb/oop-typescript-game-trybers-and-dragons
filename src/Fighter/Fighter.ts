db9420ff38c2fef78cf9fb2004ba621dbcf6602d
Jean Paul Bernhardt <beralb@protonmail.com>
2022-11-19 16:51:47 -0300

    refatoracao req 06

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,21 +1,21 @@
 import Energy from '../Energy';
 
-// interface Fighter {
-//   lifePoints: number;
-//   strength: number;
-//   defense: number;
-//   energy?: Energy;
-//   attack(enemy: Fighter): void;
-//   special?(enemy: Fighter): void;
-//   levelUp(): void;
-//   receiveDamage(attackPoints: number): number;
-// }
-
 interface Fighter {
+  lifePoints: number;
+  strength: number;
   defense: number;
   energy?: Energy;
+  attack(enemy: Fighter): void;
   special?(enemy: Fighter): void;
   levelUp(): void;
+  receiveDamage(attackPoints: number): number;
 }
+
+// interface Fighter {
+//   defense: number;
+//   energy?: Energy;
+//   special?(enemy: Fighter): void;
+//   levelUp(): void;
+// }
 
 export default Fighter;
